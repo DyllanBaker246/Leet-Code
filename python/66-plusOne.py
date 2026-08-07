643a9131a5bb7dcf0871c23f45347d888bc32cd6
dyllanbaker246 <dyllanbakerpanda@gmail.com>
2026-08-07 11:44:58 -0700

    finished 66

---
+++ python/66-plusOne.py
@@ -0,0 +1,83 @@
+'''
+66. Plus One
+Easy
+Topics
+premium lock icon
+Companies
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+ 
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+Example 2:
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+Example 3:
+
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
+ 
+
+Constraints:
+
+1 <= digits.length <= 100
+0 <= digits[i] <= 9
+digits does not contain any leading 0's.
+'''
+import array as arr
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        nums = digits[::-1] # reverse array
+        current = nums[0] + 1 # plus one
+        if current != 10:
+            nums[0] = current
+            return nums[::-1] 
+
+        carry = 1
+        current = 0
+        nums[0] = 0
+        i = 1 # start at second index
+        while i < len(nums):
+            if carry == 1:
+                current = nums[i] + 1
+                if current == 10:
+                    nums[i] = 0
+                    carry = 1
+                    i += 1
+                    continue
+                
+                nums[i] = current
+                carry = 0
+                break
+            break
+        
+        if carry == 1:
+            nums.append(1)
+        return nums[::-1]
+
+def main():
+    solution = Solution()
+    nums = [9,9,9,9]
+    nums = solution.plusOne(nums)
+
+    # print(len(nums))
+    for item in nums:
+        print(item, end=' ')
+    print("")
+
+main()
